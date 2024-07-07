eda62300fcc2f20f5a16135fd2cff30d0d86c821
Tmyyy <zx16689532246@163.com>
2024-07-08 03:47:52 +0800

    main：5.5-基本实现，牛逼瀑布流

--- src/main/java/com/example/mirror/config/WebConfig.java
+++ src/main/java/com/example/mirror/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8080") // 允许的前端地址
+                        .allowedOrigins("http://localhost:8086") // 允许的前端地址
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
